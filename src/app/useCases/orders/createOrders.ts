4e6f6f56291d5bcb01d82996667277ba22d7fca6
Caio França <caiobdafranca@gmail.com>
2022-11-19 21:08:08 -0300

    ✨feat: Adiciona web socket

--- src/app/useCases/orders/createOrders.ts
+++ src/app/useCases/orders/createOrders.ts
@@ -1,5 +1,6 @@
 import { Order } from "./../../models/Order";
 import { Request, Response } from "express";
+import { io } from "../../..";
 
 export async function createOrder(req: Request, res: Response) {
   try {
@@ -8,6 +9,10 @@ export async function createOrder(req: Request, res: Response) {
       table,
       products,
     });
+
+    const orderDetails = await order.populate("products.product");
+
+    io.emit("orders@new", orderDetails);
 
     res.status(201).json(order);
   } catch (error) {
